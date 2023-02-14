a68529e65d19996aedb033c914da8d89d6d1cd55
Michael Hayes <michael.hayes@canterbury.ac.nz>
2023-02-14 14:03:41 +1300

    Add connection

---
+++ lcapygui/components/connection.py
@@ -0,0 +1,28 @@
+from .component import Component
+from numpy import array
+
+
+class Connection(Component):
+
+    def __init__(self):
+
+        super().__init__(None)
+
+    @property
+    def midpoint(self) -> array:
+        """
+        Computes the midpoint of the component.
+        """
+
+        return array(self.nodes[0].position)
+
+    def length(self) -> float:
+        """
+        Computes the length of the component.
+        """
+        return 0.5
+
+    def assign_positions(self, x1, y1, x2, y2) -> array:
+        """Assign node positions based on cursor positions."""
+
+        return array(((x1, y1), ))
