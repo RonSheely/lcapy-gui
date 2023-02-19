7e52ea28f0cb4f9959e340f5ff2c3dd051c322dd
Michael Hayes <michael.hayes@canterbury.ac.nz>
2023-02-19 16:33:48 +1300

    Hack opamp drawing

--- lcapygui/components/opamp.py
+++ lcapygui/components/opamp.py
@@ -55,15 +55,22 @@ class Opamp(Component):
         return path
 
     def assign_positions(self, x1, y1, x2, y2) -> array:
-        """Assign node positions based on cursor positions."""
+        """Assign node positions based on cursor positions.
 
-        length = sqrt((x2 - x1)**2 + (y2 - y1)**2)
+        x1, y1 defines the positive input node
+        x2, y2 defines the negative input node"""
+
+        r = sqrt((x2 - x1)**2 + (y2 - y1)**2)
 
         # TODO: handle rotation
-        positions = array(((x2, y2),
+
+        xo = (x2 + x1) / 2
+        yo = r * 4 / 5
+
+        positions = array(((xo, yo),
                            (0, 0),
-                           (x1, y2 + length / 5),
-                           (x1, y2 - length / 5)))
+                           (x1, y1),
+                           (x1, y2)))
         return positions
 
     @property
@@ -79,3 +86,23 @@ class Opamp(Component):
 
         diff = (pos - self.nodes[0].position) / 2
         return norm(diff)
+
+    def net(self, connections, step=1):
+
+        nodenames = [node.name for node in self.nodes]
+
+        # TODO: Handle rotation
+        dy = abs(self.nodes[3].y - self.nodes[2].y)
+        size = dy * 5 / 4
+
+        return '%s %s %s opamp %s %s; right=%s' % (self.name, *nodenames, size)
+
+    def draw(self, editor, layer, **kwargs):
+
+        x1, y1 = self.nodes[2].position
+        x2, y2 = self.nodes[3].position
+
+        xc = (x1 + x2) / 2
+        yc = (y1 + y2) / 2
+
+        self.sketch.draw(layer.ax, offset=(xc, yc), angle=0, lw=2, **kwargs)
