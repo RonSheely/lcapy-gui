05cd8770203d73683673b87be7b1104e26e59975
Michael Hayes <michael.hayes@canterbury.ac.nz>
2023-02-22 13:21:52 +1300

    V0.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
         "lcapygui",
         "lcapygui.*"
     ]),
-    version="v0.2.0",
+    version="0.3",
     description="A GUI for lcapy",
     long_description=long_description,
     long_description_content_type="text/markdown",
