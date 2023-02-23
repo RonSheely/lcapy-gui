61fe0f00c39e48559c0e0536c25ed6a8acbb1198
Michael Hayes <michael.hayes@canterbury.ac.nz>
2023-02-23 14:05:31 +1300

    V0.3.10

--- lcapygui/__init__.py
+++ lcapygui/__init__.py
@@ -6,8 +6,12 @@ These can be imported directly from lcapy-gui.
 from .ui.tk.lcapytk import LcapyTk
 import sys
 
-from importlib.metadata import version
-__version__ = version('lcapygui')
+if sys.version_info < (3, 9):
+    import pkg_resources
+    __version__ = pkg_resources.require('lcapygui')[0].version
+else:
+    from importlib.metadata import version
+    __version__ = version('lcapygui')
 
 
 if sys.version_info < (3, 9):
