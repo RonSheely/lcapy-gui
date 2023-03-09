6a59f754b52ae6d21102c56e1faee528532e891e
Michael Hayes <michael.hayes@canterbury.ac.nz>
2023-03-10 11:24:24 +1300

    Add transistor

--- lcapygui/components/bjt.py
+++ lcapygui/components/bjt.py
@@ -1,10 +1,10 @@
-from .component import Component
+from .transistor import Transistor
 from numpy import array, sqrt, dot, degrees
 from numpy.linalg import norm
 from math import atan2
 
 
-class BJT(Component):
+class BJT(Transistor):
 
     type = "Q"
     label_offset = 0
@@ -15,46 +15,3 @@ class BJT(Component):
 
     kinds = {'': '', 'nigbt': 'nigbt', 'pigbt': 'pigbt', 'Lnigbt': 'Lnigbt',
              'Lpigbt': 'Lpigbt'}
-
-    def assign_positions(self, x1, y1, x2, y2) -> array:
-        """Assign node positions based on cursor positions.
-
-        x1, y1 defines the positive input node
-        x2, y2 defines the negative input node"""
-
-        dx = x1 - x2
-        dy = y1 - y2
-        r = sqrt(dx**2 + dy**2)
-        R = array(((dx, -dy), (dy, dx))) / r
-
-        ym = (y1 + y2) / 2
-        xm = (x1 + x2) / 2
-
-        xg, yg = dot(R.T, (0, -2))
-        xg += xm
-        yg += ym
-
-        # D G S
-        positions = array(((x1, y1),
-                           (xg, yg),
-                           (x2, y2)))
-        return positions
-
-    @property
-    def node1(self):
-
-        return self.nodes[0]
-
-    @property
-    def node2(self):
-
-        return self.nodes[2]
-
-    @property
-    def sketch_net(self):
-
-        # With up, base is down.
-        s = self.type + ' 1 2 3; up'
-        if self.kind != '':
-            s += ', kind=' + self.kind
-        return s
---
+++ lcapygui/components/transistor.py
@@ -0,0 +1,48 @@
+from .component import Component
+
+
+class Transistor(Component):
+
+        def assign_positions(self, x1, y1, x2, y2) -> array:
+        """Assign node positions based on cursor positions.
+
+        x1, y1 defines the positive input node
+        x2, y2 defines the negative input node"""
+
+        dx = x1 - x2
+        dy = y1 - y2
+        r = sqrt(dx**2 + dy**2)
+        R = array(((dx, -dy), (dy, dx))) / r
+
+        # Midpoint
+        ym = (y1 + y2) / 2
+        xm = (x1 + x2) / 2
+
+        xg, yg = dot(R.T, (0, -2))
+        xg += xm
+        yg += ym
+
+        # D G S
+        positions = array(((x1, y1),
+                           (xg, yg),
+                           (x2, y2)))
+        return positions
+
+    @property
+    def node1(self):
+
+        return self.nodes[0]
+
+    @property
+    def node2(self):
+
+        return self.nodes[2]
+
+    @property
+    def sketch_net(self):
+
+        # With up, drain is down.
+        s = self.type + ' 1 2 3; up'
+        if self.kind != '':
+            s += ', kind=' + self.kind
+        return s
