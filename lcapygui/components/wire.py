f2b9ba5e5d1ed3a002fd6e2224bb41f7894e4b89
Michael Hayes <michael.hayes@canterbury.ac.nz>
2023-03-15 17:07:21 +1300

    Allow shorter wires

--- lcapygui/components/wire.py
+++ lcapygui/components/wire.py
@@ -6,3 +6,11 @@ class Wire(BipoleComponent):
     type = 'W'
     args = ()
     sketch_net = 'W 1 2'
+
+    def draw(self, editor, sketcher, **kwargs):
+
+        x1, y1 = self.node1.x, self.node1.y
+        x2, y2 = self.node2.x, self.node2.y
+
+        kwargs = self.make_kwargs(editor, **kwargs)
+        sketcher.stroke_line(x1, y1, x2, y2, **kwargs)
