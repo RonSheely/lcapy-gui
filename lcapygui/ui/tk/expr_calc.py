4e0060d33c151fdd8babb9acd1278960b62f3f02
Michael Hayes <michael.hayes@canterbury.ac.nz>
2023-06-18 21:20:44 +1200

    Add expr_calc

---
+++ lcapygui/ui/tk/expr_calc.py
@@ -0,0 +1,17 @@
+global_dict = {}
+exec('from lcapy import *', global_dict)
+
+
+class ExprCalc:
+
+    def __init__(self, expr):
+
+        self.expr = expr
+
+    def method(self, name):
+
+        global_dict['result'] = self.expr
+
+        command = '(result).%s()' % name
+        expr = eval(command, global_dict)
+        return expr
