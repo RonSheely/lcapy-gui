55f7ba12dcfaa916c5c6c4213df8d845a631ef74
Michael Hayes <michael.hayes@canterbury.ac.nz>
2023-07-20 18:51:07 +1200

    Remove padding

--- lcapygui/ui/tk/image_dialog.py
+++ lcapygui/ui/tk/image_dialog.py
@@ -13,27 +13,13 @@ class ImageDialog(Tk):
 
         image = Image.open(filename)
 
-        pad = 30
-        right = pad
-        left = pad
-        top = pad
-        bottom = pad
-
         width, height = image.size
 
-        new_width = width + right + left
-        new_height = height + top + bottom
+        label = Label(self, text='', width=image.width,
+                      height=image.height)
+        label.pack(fill=BOTH, expand=True)
 
-        # Add border
-        background = (245, 245, 245)
-        image_pad = Image.new(image.mode, (new_width, new_height), background)
-        image_pad.paste(image, (left, top))
+        img = ImageTk.PhotoImage(image, master=self)
 
-        expr_label = Label(self, text='', width=image.width + 100,
-                           height=image.height + 100)
-        expr_label.pack(fill=BOTH, expand=True)
-
-        img_pad = ImageTk.PhotoImage(image_pad, master=self)
-
-        expr_label.config(image=img_pad)
-        expr_label.photo = img_pad
+        label.config(image=img)
+        label.photo = img
