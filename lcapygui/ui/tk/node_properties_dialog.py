2840a6b297208fb801bf891e2ffbb4b624cd4694
Michael Hayes <michael.hayes@canterbury.ac.nz>
2023-08-08 08:26:59 +1200

    Use Window for node properties

--- lcapygui/ui/tk/node_properties_dialog.py
+++ lcapygui/ui/tk/node_properties_dialog.py
@@ -1,50 +1,38 @@
 from tkinter import Tk, StringVar, Label, Entry, Button
+from .window import Window
 
 
-class NodePropertiesDialog:
+class NodePropertiesDialog(Window):
 
     def __init__(self, ui, node, update=None, title=''):
 
-        self.ui = ui
+        # FIXME if have node name same as a cpt name
+        super(NodePropertiesDialog, self).__init__(ui, node.name, title)
+
         self.node = node
         self.update = update
 
-        self.window = Tk()
-        self.window.title(title)
-
         row = 0
 
-        self.name_var = StringVar(self.window)
+        self.name_var = StringVar(self)
         self.name_var.set(node.name)
 
-        name_label = Label(self.window, text='Name: ')
-        name_entry = Entry(self.window, textvariable=self.name_var)
+        name_label = Label(self, text='Name: ')
+        name_entry = Entry(self, textvariable=self.name_var)
 
         name_label.grid(row=row)
         name_entry.grid(row=row, column=1)
         row += 1
 
-        button = Button(self.window, text="OK", command=self.on_update)
+        button = Button(self, text="OK", command=self.on_ok)
         button.grid(row=row)
 
-    def focus(self):
-
-        self.window.focus()
-
-        # Put window on top
-        self.window.attributes('-topmost', True)
-
-    def on_close(self):
-
-        self.ui.dialogs.pop(self.cpt.name)
-        self.window.destroy()
-
-    def on_update(self):
+    def on_ok(self):
 
         node_name = self.name_var.get()
-
-        self.window.destroy()
 
         if self.node.name != node_name and self.update:
             self.node.name = node_name
             self.update(self.node)
+
+        self.on_close()
