661f4be7a9f05d95d278b6ef275340944d443d5b
Bailey Allen <bailey@blowninto.space>
2023-11-20 12:22:57 +1300

    Added some comments to uimodelmph.py

--- lcapygui/ui/uimodeldnd.py
+++ lcapygui/ui/uimodeldnd.py
@@ -5,3 +5,15 @@ class UIModelDnD(UIModelMPH):
 
     def __init__(self, ui):
         super().__init__(ui)
+        self.mouse_pos = (0 ,0)
+
+    def on_add_cpt(self, cpt_key):
+        """
+        Adds a component to the circuit after a key is pressed
+        :param cpt_key: key pressed
+        :return: None
+        """
+        super().on_add_cpt(cpt_key)
+
+
+
