a35b4a567e0306774963e1169cd60ecc060f3b1c
Michael Hayes <michael.hayes@canterbury.ac.nz>
2024-01-03 08:49:43 +1300

    Fix default diode

--- lcapygui/components/diode.py
+++ lcapygui/components/diode.py
@@ -5,11 +5,11 @@ class Diode(Bipole):
 
     type = 'D'
     args = ()
-    kinds = {'-': '', '-led': 'LED', '-photo': 'Photo', '-schottky': 'Schottky',
+    kinds = {'': '', '-led': 'LED', '-photo': 'Photo', '-schottky': 'Schottky',
              '-zener': 'Zener', '-zzener': 'Zzener', '-tunnel': 'Tunnel',
              '-varcap': 'VarCap', '-bidirectional': 'Bidirectional',
              '-tvs': 'TVS', '-laser': 'Laser'}
     styles = {'empty': 'Empty', 'full': 'Full', 'stroke': 'Stroke'}
-    default_kind = '-'
+    default_kind = ''
     default_style = 'empty'
     has_value = False
